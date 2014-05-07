7b33ed354d2a67a5a09ab2a83ce181a65404aaaf
gongzhaohui <gongzhaohui@gmail.com>
2014-05-07 23:32:05 +0800

    2014-5-5 async recursion test

--- server/controllers/test.js
+++ server/controllers/test.js
@@ -283,12 +283,11 @@ exports.testPromise = function (req, res) {
     });
     User.create(users, function (err, docs) {
        var bom=[];
-        var Q=require('q');
         var getChildren=function(lvl,parent,qty,bom,seq){
-        return User.find({ name: parent }).lean().populate({path:'children.child',select:'name'}).exec().then(function(err,inv) {
-            return Q.all(function(inv) {
+         User.find({ name: parent }).lean().populate({path:'children.child',select:'name'}).exec().then(function(err,inv) {
                 seq=seq||'0';
                 bom.push({lvl: lvl,seq:seq, parent: inv.name,qty:qty});
+            console.log(lvl);
                 if (inv.children.length>0) {
                     lvl++;
                     children.each(function (child) {
@@ -301,10 +300,10 @@ exports.testPromise = function (req, res) {
                 }
 
             });
-        });
         };
 
-        getChildren(0,'p0',2,bom).all(function(err){
+
+        getChildren(0,'p0',2,bom).then(function(err){
             res.jsonp(bom);
         });
 
