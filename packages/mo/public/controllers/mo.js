838d53514666c0c7fde3d5c5832838610aea3f4d
gongzhaohui <gongzhaohui@gmail.com>
2014-05-12 16:59:37 +0800

    20140508 mean upgrade

--- packages/mo/public/controllers/mo.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('mean').controller('MoController', ['$scope', 'Global',
-  function($scope, Global, Mo) {
-    $scope.global = Global;
-    $scope.mo = {name:'mo'};
-
-  }
-]);
