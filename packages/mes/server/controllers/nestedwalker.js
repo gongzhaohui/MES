e4c6f278331955447fec483e2c25591eb7b9c036
gongzhaohui <gongzhaohui@gmail.com>
2014-05-22 16:30:09 +0800

    20140520 test promise

---
+++ packages/mes/server/controllers/nestedwalker.js
@@ -0,0 +1,233 @@
+/**
+ * referred Async Walker By bfricka
+ * 2014-5-22
+ * Gong
+ */
+var _ = require('lodash');
+var q = require('q');
+var fs = require('fs');
+var path = require('path');
+var chalk = require('chalk');
+
+var defaultConfig = {
+
+    callback: null,
+    maxDepth: 1000,
+    maxIterations: 500,
+    assemblyMode: false
+};
+
+var pathsep='.';
+
+/**
+ * Main module function. Performs all async walking
+ * @param  {string} model               - model to walk
+ * @param  {string} fieldValue               - Path to walk
+ * @param  {string} fieldValue               - Path to walk
+ * @param  {object|function} [config] - Optional config object or callback
+ * @param  {boolean} [assemblyMode]  - Optional param to indicate directory instead of file mode
+ * @return {object}                   - Q.promise that resolves to results array.
+ */
+function asyncWalker(model,field,fieldValue, config, assemblyMode) {
+    var defaults = _.clone(defaultConfig);
+
+    if (config) {
+        if (_.isFunction(config)) {
+            defaults.callback = config;
+            config = defaults;
+
+            if (arguments.length > 2) config.assemblyMode = assemblyMode;
+        } else {
+            config = _.defaults(config, defaults);
+        }
+    } else {
+        config = defaults;
+    }
+
+    var originalDepth = 0;
+    var hasMaxDepth = _.isFinite(config.maxDepth);
+    var iterations = 0;
+    var hasMaxIterations = _.isFinite(config.maxIterations);
+    var hasRecursionLimits = hasMaxDepth || hasMaxDepth;
+
+    /**
+     * Get current path depth
+     * @param  {string} dir - Directory path
+     * @return {number}     - Current depth
+     */
+    function getDepth(path) {
+        return path.split(pathsep).length;
+    }
+
+    function getDepthFromBase(path) {
+        return getDepth(path) - originalDepth;
+    }
+
+    /**
+     * Main asyncEntry
+     * @param  {string} fieldValue - Qualified value to search
+     * @return {object}     - Promise
+     */
+    function walkAsync(fieldValue,path) {
+        var skip = false;
+        var deferred = q.defer();
+        path=path||'';
+        if (hasRecursionLimits) {
+            var currentDepth = getDepthFromBase(path);
+            if (hasMaxDepth && currentDepth >= config.maxDepth) {
+                console.warn(chalk.yellow("Reached max depth (%d) in: " + dir), config.maxDepth);
+                skip = true;
+            }
+
+            if (hasMaxIterations) {
+                if (iterations >= config.maxIterations) skip = true;
+                iterations++;
+            }
+        }
+
+        if (skip) {
+            deferred.resolve(void 0);
+        } else {
+            readDoc(model,field,fieldValue)
+                .then(statItems)
+                .then(getFiles)
+                .then(function(a) {
+                    deferred.resolve(a);
+                }, function(b) {
+                    deferred.reject("Error " + b);
+                });
+        }
+
+        return deferred.promise;
+    }
+
+    /**
+     * Async readDoc that makes sure iterations doesn't get out of hand
+     * maps directory contents to the qualified base, and returns
+     * a promise intead of using callback style
+     * @param  {string} fieldValue - Qualified value to search
+     * @return {object}     - fs.readDoc wrapped in a promise
+     */
+    function readDoc(fieldValue) {
+        return q
+            .nfapply(fs.readdir, [dir])
+            .then(function(list) {
+                return mapPaths(dir, list);
+            });
+    }
+
+    /**
+     * Gets the fs.stat object from each item in the list and
+     * sends back an object w/ the relevant values set
+     * @param  {Array.<string>} list - List of paths to get fs.stat info for
+     * @return {object}     - Promise that resolves to the array of normalized
+     * objects when all fs.stat promises resolve
+     */
+    function statItems(list) {
+        var statPromises = [];
+
+        _.forEach(list, function(item) {
+            var qStatPromise = q.nfapply(fs.stat, [item]).then(function(stat) {
+                var itemStat = {
+                    path: item,
+                    isFile: stat.isFile(),
+                    isDirectory: stat.isDirectory()
+                };
+
+                if (config.callback) {
+                    itemStat = config.callback(itemStat);
+                    // For filter
+                    if (!itemStat) return;
+                }
+
+                return itemStat;
+            }, function(err) {
+                if (err) console.warn(chalk.yellow(err.message));
+            });
+
+            statPromises.push(qStatPromise);
+        });
+
+        return q.all(statPromises);
+    }
+
+    /**
+     * Takes in a stat list and collects the files, while creating new
+     * promises on any directories. When all promises resolve down the chain
+     * this eventually has the list of all files.
+     * @param  {array} list - List of stat objects
+     * @return {object}     - q.all promise that resolves w/ the full
+     * list of files when all promises have been resolved
+     */
+    function getFiles(list) {
+        var files = [];
+        var promises = [];
+
+        _.forEach(list, function(item) {
+            if (!item) return;
+
+            if (config.assemblyMode) {
+                if (item.isDirectory) {
+                    files.push(item.path);
+                    promises.push(walkAsync(item.path));
+                }
+                return;
+            }
+
+            if (item.isDirectory) {
+                promises.push(walkAsync(item.path));
+            } else if (item.isFile) {
+                files.push(item.path);
+            }
+        });
+
+        return q.all(promises).then(function(list) {
+            return _.flatten(files.concat(_.compact(list)));
+        });
+    }
+
+    /**
+     * Simply takes a list of items and joins the provided base to each item.
+     * @param  {string} base - The base path to use
+     * @param  {array} list - List of items to use
+     * @return {array}      - Mapped list of items
+     */
+    function mapPaths(base, list) {
+        return _.map(list, function(item) {
+            var paths = Array.prototype.filter.call(arguments, f);
+            var joined = paths.join('\\');
+            return path.join(base, item);
+        });
+    }
+
+    return walkAsync(fieldValue,path);
+}
+
+(function(aw) {
+    function _createFilterFn(callback, matchDirectories) {
+        return function(itemStat) {
+            var toAdd = (itemStat.isDirectory && !matchDirectories) ? true : !!callback(itemStat.path);
+            return toAdd ? itemStat : void 0;
+        };
+    }
+
+    function _createMapFn(callback) {
+        return function(itemStat) {
+            if (itemStat.isDirectory) return itemStat;
+            itemStat.path = callback(itemStat.path);
+            return itemStat;
+        };
+    }
+
+    aw.filter = function(dir, callback, matchDirectories) {
+        var filterFn = _createFilterFn(callback, matchDirectories);
+        return aw(dir, filterFn);
+    };
+
+    aw.map = function(dir, callback) {
+        var mapFn = _createMapFn(callback);
+        return aw(dir, mapFn);
+    };
+}(asyncWalker));
+
+module.exports = asyncWalker;
--- packages/mes/server/controllers/test.js
+++ packages/mes/server/controllers/test.js
@@ -192,15 +192,25 @@ exports.testPromise = function (req, res) {
        }
     };
 */
-    function getFromRedis(parent){
-        return User.findOne({ name: parent}).lean().populate({path: 'children.child', select: 'name'}).then(function (inv) {
+
+    var getusr= Q.nbind(User.findOne,User);
+    var popusr= Q.nbind(User.populate,User);
+    getusr({ 'name': 'p0'}).then(function(usr){
+        console.log(usr);
+        popusr(usr,{path: 'children.child', select: 'name'}).then(function(usr){
+            res.jsonp(usr);
+        })
+    });
+    /*function spread(parent){
+        var promise=User.findOne({ 'name': parent}).lean().populate({path: 'children.child', select: 'name'}).exec();
+        return promise.then(function (inv) {
             // this is our returned object
             var constructedObject = {label: inv.name};
 
             if (inv.children.length) {
                 var dependents = inv.children.map(function(par) {
                     // get a promise for the next level
-                    return getFromRedis(par.name);
+                    return spread(par.name);
                 });
                 return Q.all(dependents).then(function(dependentResults) {
                     constructedObject.parents = dependentResults;
@@ -212,11 +222,11 @@ exports.testPromise = function (req, res) {
         });
     }
 
-    getFromRedis( 'p0' ).done(function(out) {
+    spread( 'p0' ).then(function(out) {
         res.jsonp( JSON.stringify( out ));
     });
    // res.jsonp(recursive([],'p0'));
-    //res.json("aaa")
+    //res.json("aaa")*/
 };
 
 
@@ -237,3 +247,229 @@ exports.update = function (req, res) {
  */
 exports.remove = function (req, res) {
 };
+
+/*-----------------------------------------------------------------
+ var _ = require('lodash');
+ var q = require('q');
+ var fs = require('fs');
+ var path = require('path');
+ var chalk = require('chalk');
+
+ var defaultConfig = {
+ callback: null,
+ maxDepth: Infinity,
+ maxIterations: 500,
+ directoryMode: false
+ };
+
+ /**
+ * Main module function. Performs all async walking
+ * @param  {string} dir               - Path to walk
+ * @param  {object|function} [config] - Optional config object or callback
+ * @param  {boolean} [directoryMode]  - Optional param to indicate directory instead of file mode
+ * @return {object}                   - Q.promise that resolves to results array.
+ */
+function asyncWalker(dir, config, directoryMode) {
+    dir = path.normalize(dir); // Normalize so depth doesn't fail
+    // Setup and normalize configuration
+    var defaults = _.clone(defaultConfig);
+
+    if (config) {
+        if (_.isFunction(config)) {
+            defaults.callback = config;
+            config = defaults;
+
+            if (arguments.length > 2) config.directoryMode = directoryMode;
+        } else {
+            config = _.defaults(config, defaults);
+        }
+    } else {
+        config = defaults;
+    }
+
+    var originalDepth = getDepth(dir);
+    var hasMaxDepth = _.isFinite(config.maxDepth);
+    var iterations = 0;
+    var hasMaxIterations = _.isFinite(config.maxIterations);
+    var hasRecursionLimits = hasMaxDepth || hasMaxDepth;
+
+    /**
+     * Get current path depth
+     * @param  {string} dir - Directory path
+     * @return {number}     - Current depth
+     */
+    function getDepth(dir) {
+        return dir.split(path.sep).length;
+    }
+
+    function getDepthFromBase(dir) {
+        return getDepth(dir) - originalDepth;
+    }
+
+    /**
+     * Main asyncEntry
+     * @param  {string} dir - Qualified directory to search
+     * @return {object}     - Promise
+     */
+    function walkAsync(dir) {
+        var skip = false;
+        var deferred = q.defer();
+
+        if (hasRecursionLimits) {
+            var currentDepth = getDepthFromBase(dir);
+            if (hasMaxDepth && currentDepth >= config.maxDepth) {
+                console.warn(chalk.yellow("Reached max depth (%d) in: " + dir), config.maxDepth);
+                skip = true;
+            }
+
+            if (hasMaxIterations) {
+                if (iterations >= config.maxIterations) skip = true;
+                iterations++;
+            }
+        }
+
+        if (skip) {
+            deferred.resolve(void 0);
+        } else {
+            readdir(dir)
+                .then(statItems)
+                .then(getFiles)
+                .then(function(a) {
+                    deferred.resolve(a);
+                }, function(b) {
+                    deferred.reject("Error " + b);
+                });
+        }
+
+        return deferred.promise;
+    }
+
+    /**
+     * Async readdir that makes sure iterations doesn't get out of hand
+     * maps directory contents to the qualified base, and returns
+     * a promise intead of using callback style
+     * @param  {string} dir - Directory to read
+     * @return {object}     - fs.readdir wrapped in a promise
+     */
+    function readdir(dir) {
+        return q
+            .nfapply(fs.readdir, [dir])
+            .then(function(list) {
+                return mapPaths(dir, list);
+            });
+    }
+
+    /**
+     * Gets the fs.stat object from each item in the list and
+     * sends back an object w/ the relevant values set
+     * @param  {Array.<string>} list - List of paths to get fs.stat info for
+     * @return {object}     - Promise that resolves to the array of normalized
+     * objects when all fs.stat promises resolve
+     */
+    function statItems(list) {
+        var statPromises = [];
+
+        _.forEach(list, function(item) {
+            var qStatPromise = q.nfapply(fs.stat, [item]).then(function(stat) {
+                var itemStat = {
+                    path: item,
+                    isFile: stat.isFile(),
+                    isDirectory: stat.isDirectory()
+                };
+
+                if (config.callback) {
+                    itemStat = config.callback(itemStat);
+                    // For filter
+                    if (!itemStat) return;
+                }
+
+                return itemStat;
+            }, function(err) {
+                if (err) console.warn(chalk.yellow(err.message));
+            });
+
+            statPromises.push(qStatPromise);
+        });
+
+        return q.all(statPromises);
+    }
+
+    /**
+     * Takes in a stat list and collects the files, while creating new
+     * promises on any directories. When all promises resolve down the chain
+     * this eventually has the list of all files.
+     * @param  {array} list - List of stat objects
+     * @return {object}     - q.all promise that resolves w/ the full
+     * list of files when all promises have been resolved
+     */
+    function getFiles(list) {
+        var files = [];
+        var promises = [];
+
+        _.forEach(list, function(item) {
+            if (!item) return;
+
+            if (config.directoryMode) {
+                if (item.isDirectory) {
+                    files.push(item.path);
+                    promises.push(walkAsync(item.path));
+                }
+                return;
+            }
+
+            if (item.isDirectory) {
+                promises.push(walkAsync(item.path));
+            } else if (item.isFile) {
+                files.push(item.path);
+            }
+        });
+
+        return q.all(promises).then(function(list) {
+            return _.flatten(files.concat(_.compact(list)));
+        });
+    }
+
+    /**
+     * Simply takes a list of items and joins the provided base to each item.
+     * @param  {string} base - The base path to use
+     * @param  {array} list - List of items to use
+     * @return {array}      - Mapped list of items
+     */
+    function mapPaths(base, list) {
+        return _.map(list, function(item) {
+            return path.join(base, item);
+        });
+    }
+
+    return walkAsync(dir);
+}
+
+(function(aw) {
+    function _createFilterFn(callback, matchDirectories) {
+        return function(itemStat) {
+            var toAdd = (itemStat.isDirectory && !matchDirectories) ? true : !!callback(itemStat.path);
+            return toAdd ? itemStat : void 0;
+        };
+    }
+
+    function _createMapFn(callback) {
+        return function(itemStat) {
+            if (itemStat.isDirectory) return itemStat;
+            itemStat.path = callback(itemStat.path);
+            return itemStat;
+        };
+    }
+
+    aw.filter = function(dir, callback, matchDirectories) {
+        var filterFn = _createFilterFn(callback, matchDirectories);
+        return aw(dir, filterFn);
+    };
+
+    aw.map = function(dir, callback) {
+        var mapFn = _createMapFn(callback);
+        return aw(dir, mapFn);
+    };
+}(asyncWalker));
+
+module.exports = asyncWalker;
+/*----------------------------------------------------- */
