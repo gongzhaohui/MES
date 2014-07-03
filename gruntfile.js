94676b474d7f9d1a22bbccce1c27ccd6800af35d
gongzhaohui <gongzhaohui@gmail.com>
2014-07-03 11:17:26 +0800

    mean 0.4.0
    Signed-off-by: gongzhaohui <gongzhaohui@gmail.com>

--- gruntfile.js
+++ gruntfile.js
@@ -1,9 +1,9 @@
 'use strict';
 
 var paths = {
-    js: ['*.js', 'server/**/*.js', 'public/**/*.js', 'test/**/*.js', '!test/coverage/**', '!public/system/lib/**', 'packages/**/*.js', '!packages/**/node_modules/**'],
-    html: ['public/**/views/**', 'server/views/**', 'packages/**/public/**/views/**', 'packages/**/server/views/**'],
-    css: ['public/**/css/*.css', '!public/system/lib/**', 'packages/**/public/**/css/*.css']
+    js: ['*.js', 'test/**/*.js', '!test/coverage/**', '!bower_components/**', 'packages/**/*.js', '!packages/**/node_modules/**'],
+    html: ['packages/**/public/**/views/**', 'packages/**/server/views/**'],
+    css: ['!bower_components/**', 'packages/**/public/**/css/*.css']
 };
 
 module.exports = function(grunt) {
@@ -15,8 +15,8 @@ module.exports = function(grunt) {
     // Project Configuration
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        assets: grunt.file.readJSON('server/config/assets.json'),
-        clean: ['public/build'],
+        assets: grunt.file.readJSON('config/assets.json'),
+        clean: ['bower_components/build'],
         watch: {
             js: {
                 files: paths.js,
@@ -71,13 +71,10 @@ module.exports = function(grunt) {
                 script: 'server.js',
                 options: {
                     args: [],
-                    ignore: ['public/**', 'node_modules/**'],
+                    ignore: ['node_modules/**'],
                     ext: 'js,html',
                     nodeArgs: ['--debug'],
                     delayTime: 1,
-                    env: {
-                        PORT: require('./server/config/config').port
-                    },
                     cwd: __dirname
                 }
             }
@@ -91,9 +88,14 @@ module.exports = function(grunt) {
         mochaTest: {
             options: {
                 reporter: 'spec',
-                require: 'server.js'
+                require: [
+                    'server.js',
+                    function() {
+                        require('mongoose').connection.once('open', function() {});
+                    }
+                ]
             },
-            src: ['test/mocha/**/*.js', 'packages/**/test/mocha/**/*.js']
+            src: ['packages/**/server/tests/**/*.js']
         },
         env: {
             test: {
@@ -102,7 +104,7 @@ module.exports = function(grunt) {
         },
         karma: {
             unit: {
-                configFile: 'test/karma/karma.conf.js'
+                configFile: 'karma.conf.js'
             }
         }
     });
@@ -112,9 +114,9 @@ module.exports = function(grunt) {
 
     //Default task(s).
     if (process.env.NODE_ENV === 'production') {
-        grunt.registerTask('default', ['clean','cssmin', 'uglify', 'concurrent']);
+        grunt.registerTask('default', ['clean', 'cssmin', 'uglify', 'concurrent']);
     } else {
-        grunt.registerTask('default', ['clean','jshint', 'csslint', 'concurrent']);
+        grunt.registerTask('default', ['clean', 'jshint', 'csslint', 'concurrent']);
     }
 
     //Test task.
